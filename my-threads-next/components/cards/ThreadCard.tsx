d505f7e85d6138210c46dca66ca3b091841b4769
yuandasp <sitiyuandap@gmail.com>
2023-08-12 20:41:29 +0700

    add formatdate

--- my-threads-next/components/cards/ThreadCard.tsx
+++ my-threads-next/components/cards/ThreadCard.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import CopyToClipboard from "../CopyToClipBoard";
+import { formatDateString } from "@/lib/utils";
 
 interface Props {
   id: string;
@@ -90,16 +91,36 @@ const ThreadCard = ({
                   />
                 </CopyToClipboard>
               </div>
-              {comments.length > 0 && (
-                <Link href={`/thread/${id}`}>
-                  <p className="mt-1 text-subtle-medium text-gray-1">
-                    {comments.length} replies
-                  </p>
-                </Link>
-              )}
+              <p className="text-subtle-medium text-gray-1 mt-2">
+                {formatDateString(createdAt)}
+              </p>
             </div>
           </div>
         </div>
+      </div>
+      <div>
+        {comments.length > 0 && (
+          <div className="ml-1 mt-3 flex items-center gap-2">
+            {comments.slice(0, 2).map((comment, index) => (
+              <Image
+                key={index}
+                src={comment.author.image}
+                alt={`user_${index}`}
+                width={24}
+                height={24}
+                className={`${
+                  index !== 0 && "-ml-5"
+                } rounded-full object-cover`}
+              />
+            ))}
+
+            <Link href={`/thread/${id}`}>
+              <p className="mt-1 text-subtle-medium text-gray-1">
+                {comments.length} repl{comments.length > 1 ? "ies" : "y"}
+              </p>
+            </Link>
+          </div>
+        )}
       </div>
     </article>
   );
